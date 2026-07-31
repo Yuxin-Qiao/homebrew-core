90548d569386298b534a8f6914707fcfb2ca87ef
Mike McQuaid <mike@mikemcquaid.com>
2026-07-31 17:30:05 +0100

    i2pd: align install step names.

--- Formula/i/i2pd.rb
+++ Formula/i/i2pd.rb
@@ -45,10 +45,11 @@ class I2pd < Formula
 
   post_install_steps do
     # Create symlinks to certificates and configs
-    ln_sf "certificates", "lib/i2pd/certificates", source_base: :opt_pkgshare, target_base: :var
-    ln_sf "i2pd.conf", "lib/i2pd/i2pd.conf", source_base: :pkgetc, target_base: :var
-    ln_sf "subscriptions.txt", "lib/i2pd/subscriptions.txt", source_base: :pkgetc, target_base: :var
-    ln_sf "tunnels.conf", "lib/i2pd/tunnels.conf", source_base: :pkgetc, target_base: :var
+    symlink "certificates", "lib/i2pd/certificates", source_base: :opt_pkgshare, target_base: :var, overwrite: true
+    symlink "i2pd.conf", "lib/i2pd/i2pd.conf", source_base: :pkgetc, target_base: :var, overwrite: true
+    symlink "subscriptions.txt", "lib/i2pd/subscriptions.txt", source_base: :pkgetc, target_base: :var,
+overwrite: true
+    symlink "tunnels.conf", "lib/i2pd/tunnels.conf", source_base: :pkgetc, target_base: :var, overwrite: true
   end
 
   service do
